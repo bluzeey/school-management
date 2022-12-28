05c696b072c2593d2101f08c085b269d96727313
Sahil Maheshwari <sahilm1711@gmail.com>
2022-12-28 14:07:25 +0530

    Update Sidebar.jsx

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,15 +11,15 @@ export default function Sidebar({user}) {
                     </div>
                     <div className="flex-1">
                         <ul className="pt-2 pb-4 space-y-1 text-md">
-                            <li className="rounded-sm">
+                            <li className="rounded-sm py-2 cursor-pointer">
                                 <Link
                                     href="/dashboard/classes"
-                                    className="flex items-center p-2 space-x-3 rounded-md"
+                                    className="flex items-center p-2 space-x-3 rounded-md "
                                 >
                                     <span>Classes</span>
                                 </Link>
                             </li>
-                            <li className="rounded-sm">
+                            <li className="rounded-sm py-2 cursor-pointer">
                                 <Link
                                     href="/dashboard/students"
                                     className="flex items-center p-2 space-x-3 rounded-md"
@@ -27,7 +27,7 @@ export default function Sidebar({user}) {
                                     <span>Students</span>
                                 </Link>
                             </li>
-                            <li className="rounded-sm">
+                            <li className="rounded-sm py-2 cursor-pointer">
                                 <Link
                                     href="/dashboard/teachers"
                                     className="flex items-center p-2 space-x-3 rounded-md"
@@ -36,7 +36,7 @@ export default function Sidebar({user}) {
                                 </Link>
                             </li>
                             {user=='admin' && 
-                            <li className="rounded-sm">
+                            <li className="rounded-sm py-2 cursor-pointer">
                                 <Link
                                     href="/dashboard/edit"
                                     className="flex items-center p-2 space-x-3 rounded-md"
@@ -45,7 +45,7 @@ export default function Sidebar({user}) {
                                 </Link>
                             </li>
                             }
-                            <li className="rounded-sm">
+                            <li className="rounded-sm py-2 cursor-pointer">
                                 <Link
                                     href="/"
                                     className="flex items-center p-2 space-x-3 rounded-md"
