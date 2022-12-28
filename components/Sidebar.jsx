2f069d97d5c78a59c401f272e453fbb4c6d8128a
Sahil Maheshwari <sahilm1711@gmail.com>
2022-12-28 12:49:46 +0530

    Update Sidebar.jsx

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,34 +2,34 @@ import React from "react";
 
 export default function Sidebar({user}) {
     return (
-        <div classNameName="flex">
-            <div classNameName="flex flex-col h-screen p-3 bg-white shadow w-60">
-                <div classNameName="space-y-3">
-                    <div classNameName="flex items-center">
-                        <h2 classNameName="text-xl font-bold">Dashboard</h2>
+        <div className="flex">
+            <div className="flex flex-col h-screen p-3 bg-white shadow w-60">
+                <div className="space-y-3">
+                    <div className="flex items-center">
+                        <h2 className="text-xl font-bold">Dashboard</h2>
                     </div>
-                    <div classNameName="flex-1">
-                        <ul classNameName="pt-2 pb-4 space-y-1 text-sm">
-                            <li classNameName="rounded-sm">
+                    <div className="flex-1">
+                        <ul className="pt-2 pb-4 space-y-1 text-sm">
+                            <li className="rounded-sm">
                                 <a
                                     href="/dashboard/classNamees"
-                                    classNameName="flex items-center p-2 space-x-3 rounded-md"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
                                 >
                                     <span>classNamees</span>
                                 </a>
                             </li>
-                            <li classNameName="rounded-sm">
+                            <li className="rounded-sm">
                                 <a
                                     href="/dashboard/students"
-                                    classNameName="flex items-center p-2 space-x-3 rounded-md"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
                                 >
                                     <span>Students</span>
                                 </a>
                             </li>
-                            <li classNameName="rounded-sm">
+                            <li className="rounded-sm">
                                 <a
                                     href="/dashboard/teachers"
-                                    classNameName="flex items-center p-2 space-x-3 rounded-md"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
                                 >
                                     <span>Teachers</span>
                                 </a>
