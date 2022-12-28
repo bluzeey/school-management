7cc42db2a0cd604b91b80cd5b732b50cc19b463f
Sahil Maheshwari <sahilm1711@gmail.com>
2022-12-28 14:27:51 +0530

    Update edit.js

--- pages/dashboard/edit.js
+++ pages/dashboard/edit.js
@@ -11,10 +11,10 @@ function edit() {
             <div className="px-6 py-4">
               <p className="font-bold text-xl mb-2">Edit Classes</p>
               <div className="flex">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
                   Add Class
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
                   Edit Existing Class
                 </button>
               </div>
@@ -24,10 +24,10 @@ function edit() {
             <div className="px-6 py-4">
               <p className="font-bold text-xl mb-2">Edit Teachers</p>
               <div className="flex">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
                   Add Teacher
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
                   Edit Existing Teacher
                 </button>
               </div>
@@ -37,10 +37,10 @@ function edit() {
             <div className="px-6 py-4">
               <p className="font-bold text-xl mb-2">Edit Students</p>
               <div className="flex">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
                   Add Student
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
                   Edit Existing Student
                 </button>
               </div>
